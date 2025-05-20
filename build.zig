0286615310d1a4502044981f44b1cf74818c5041
Stefan Pejić <mordnis@mordnis.org>
2025-05-20 12:50:23 +0200

    Create build.zig

---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const replxx = b.addStaticLibrary(.{
+        .name = "replxx",
+        .target = target,
+        .optimize = optimize,
+    });
+    replxx.linkLibC();
+    replxx.linkLibCpp();
+    replxx.addCSourceFiles(.{
+        .files = &.{
+            "replxx/src/ConvertUTF.cpp",
+            "replxx/src/conversion.cxx",
+            "replxx/src/escape.cxx",
+            "replxx/src/history.cxx",
+            "replxx/src/prompt.cxx",
+            "replxx/src/replxx.cxx",
+            "replxx/src/replxx_impl.cxx",
+            "replxx/src/terminal.cxx",
+            "replxx/src/util.cxx",
+            "replxx/src/wcwidth.cpp",
+            "replxx/src/windows.cxx",
+        },
+        .flags = &.{},
+    });
+    replxx.addIncludePath(b.path("replxx/src"));
+    replxx.addIncludePath(b.path("replxx/include"));
+    replxx.root_module.addCMacro("REPLXX_STATIC", "1");
+    replxx.installHeadersDirectory(b.path("replxx/include"), "", .{});
+
+    b.installArtifact(replxx);
+
+    const spp = b.addExecutable(.{
+        .name = "sqlplusplus",
+        .root_source_file = b.path("src/spp.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    spp.linkLibrary(replxx);
+
+    b.installArtifact(spp);
+}
